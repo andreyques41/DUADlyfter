b79c4e08bb4c317d6668885ef42e2a630381aae0
Andrey Quesada Mora <andreyques41@gmail.com>
2025-03-30 17:10:18 -0600

    Unittest1 for bubble sort ready

--- semana_16/test_bubblesort_ejercicio1.py
+++ semana_16/test_bubblesort_ejercicio1.py
@@ -6,24 +6,31 @@ def test_bubble_sort_returns_sorted_list_with_small_list():
     input_list = [ 2, -6, 87, 0, -879, 89, 10]
     sorted_list = [ -879, -6, 0, 2, 10, 87, 89]
     # Act
-    bubble_sort(input_list)
+    result_list = bubble_sort(input_list)
     # Assert
-    assert input_list == sorted_list
+    assert result_list == sorted_list
 
 def test_bubble_sort_returns_sorted_list_with_big_list():
     # Arrange
     input_list = [ 2, -6, 87, 0, -879, 89, 10]
     sorted_list = [ -879, -6, 0, 2, 10, 87, 89]
     # Act
-    bubble_sort(input_list)
+    result_list = bubble_sort(input_list)
     # Assert
-    assert input_list == sorted_list
+    assert result_list == sorted_list
 
 def test_bubble_sort_returns_sorted_list_with_empty_list():
-    
-    pass
+    # Arrange
+    input_list = []
+    # Act & Assert
+    with pytest.raises(ValueError, match="Input list cannot be empty"):
+        result_list = bubble_sort(input_list)
 
 def test_bubble_sort_returns_sorted_list_with_invalid_input():
-    pass
+    # Arrange
+    input_list = 's'
+    # Act & Assert
+    with pytest.raises(ValueError, match="The input must be a list of numbers"):
+        result_list = bubble_sort(input_list)
 
 
