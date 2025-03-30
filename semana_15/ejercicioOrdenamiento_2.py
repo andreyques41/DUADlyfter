f6c5e060bf0a3daba2753224b8a9b68d2a31d5a0
Andrey Quesada Mora <andreyques41@gmail.com>
2025-03-30 12:16:36 -0600

    Order exercises

--- semana_15/ejercicioOrdenamiento_2.py
+++ semana_15/ejercicioOrdenamiento_2.py
@@ -1,15 +1,15 @@
 def bubble_sort(unsorted_list: list[float] | list[int]):
-    # Outer loop to control the number of passes
-    for outer_index in range(0, len(unsorted_list) - 1):
+    # Outer loop to control the number of passes, starting from the end of the list
+    for outer_index in range(len(unsorted_list)-1, 0, -1):
         has_made_changes = False  # Flag to check if any swaps were made
-        # Inner loop to compare adjacent elements
-        for index in range(0, len(unsorted_list) - 1 - outer_index):
-            if unsorted_list[index] > unsorted_list[index+1]:  # Swap if elements are out of order
+        # Inner loop to compare and swap elements from right to left
+        for index in range(len(unsorted_list)-1, 0 + (len(unsorted_list)-1 - outer_index), -1):
+            if unsorted_list[index] < unsorted_list[index-1]:  # Swap if elements are out of order
                 temporary = unsorted_list[index]
-                unsorted_list[index] = unsorted_list[index+1]
-                unsorted_list[index+1] = temporary
+                unsorted_list[index] = unsorted_list[index-1]
+                unsorted_list[index-1] = temporary
                 has_made_changes = True
-                print(f'Iteration {outer_index,index}: \t{unsorted_list}')  # Print the list after each swap
+                print(f'Iteration {len(unsorted_list)-1-outer_index,len(unsorted_list)-1-index}: \t{unsorted_list}')  # Print the list after each swap
         if not has_made_changes:  # Exit early if no swaps were made
             return
 
