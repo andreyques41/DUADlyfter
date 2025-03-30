bbba5996ca652dd774a53271807d3cf80bd5eae0
Andrey Quesada Mora <andreyques41@gmail.com>
2025-03-30 16:25:06 -0600

    Added validator for invalid input

--- semana_15/ejercicioBigO_1.py
+++ semana_15/ejercicioBigO_1.py
@@ -1,17 +1,20 @@
 def bubble_sort(unsorted_list: list[float] | list[int]):  # O(n^2) worst case, O(n) best case
-    # Outer loop to control the number of passes
-    for outer_index in range(0, len(unsorted_list) - 1):  # O(n)
-        has_made_changes = False  # O(1)
-        # Inner loop to compare adjacent elements
-        for index in range(0, len(unsorted_list) - 1 - outer_index):  # O(n) in the worst case
-            if unsorted_list[index] > unsorted_list[index+1]:  # O(1)
-                temporary = unsorted_list[index]  # O(1)
-                unsorted_list[index] = unsorted_list[index+1]  # O(1)
-                unsorted_list[index+1] = temporary  # O(1)
-                has_made_changes = True  # O(1)
-                print(f'Iteration {outer_index,index}: \t{unsorted_list}')  # O(1) 
-        if not has_made_changes:  # O(1)
-            return
+    if not isinstance(unsorted_list, list) or not all(isinstance(item, (float, int)) for item in unsorted_list): # O(n)
+        print('The input variable is not a list of numbers!')  # O(1)
+    else:
+        # Outer loop to control the number of passes
+        for outer_index in range(0, len(unsorted_list) - 1):  # O(n)
+            has_made_changes = False  # O(1)
+            # Inner loop to compare adjacent elements
+            for index in range(0, len(unsorted_list) - 1 - outer_index):  # O(n) in the worst case
+                if unsorted_list[index] > unsorted_list[index+1]:  # O(1)
+                    temporary = unsorted_list[index]  # O(1)
+                    unsorted_list[index] = unsorted_list[index+1]  # O(1)
+                    unsorted_list[index+1] = temporary  # O(1)
+                    has_made_changes = True  # O(1)
+                    print(f'Iteration {outer_index,index}: \t{unsorted_list}')  # O(1) 
+            if not has_made_changes:  # O(1)
+                return
 
 def execute():  # O(1)
     # Initialize the list to be sorted
--- semana_15/ejercicioOrdenamiento_2.py
+++ semana_15/ejercicioOrdenamiento_2.py
@@ -1,25 +1,28 @@
 def bubble_sort(unsorted_list: list[float] | list[int]):
-    # Outer loop to control the number of passes, starting from the end of the list
-    for outer_index in range(len(unsorted_list)-1, 0, -1):
-        has_made_changes = False  # Flag to check if any swaps were made
-        # Inner loop to compare and swap elements from right to left
-        for index in range(len(unsorted_list)-1, 0 + (len(unsorted_list)-1 - outer_index), -1):
-            if unsorted_list[index] < unsorted_list[index-1]:  # Swap if elements are out of order
-                temporary = unsorted_list[index]
-                unsorted_list[index] = unsorted_list[index-1]
-                unsorted_list[index-1] = temporary
-                has_made_changes = True
-                print(f'Iteration {len(unsorted_list)-1-outer_index,len(unsorted_list)-1-index}: \t{unsorted_list}')  # Print the list after each swap
-        if not has_made_changes:  # Exit early if no swaps were made
-            return
+    if not isinstance(unsorted_list, list) or not all(isinstance(item, (float, int)) for item in unsorted_list):
+        print('The input variable is not a list of numbers!')
+    else:
+        # Outer loop to control the number of passes, starting from the end of the list
+        for outer_index in range(len(unsorted_list)-1, 0, -1):
+            has_made_changes = False  # Flag to check if any swaps were made
+            # Inner loop to compare and swap elements from right to left
+            for index in range(len(unsorted_list)-1, 0 + (len(unsorted_list)-1 - outer_index), -1):
+                if unsorted_list[index] < unsorted_list[index-1]:  # Swap if elements are out of order
+                    temporary = unsorted_list[index]
+                    unsorted_list[index] = unsorted_list[index-1]
+                    unsorted_list[index-1] = temporary
+                    has_made_changes = True
+                    print(f'Iteration {len(unsorted_list)-1-outer_index,len(unsorted_list)-1-index}: \t{unsorted_list}')  # Print the list after each swap
+            if not has_made_changes:  # Exit early if no swaps were made
+                return
 
 def execute():
     # Initialize the list to be sorted
     unsorted_list = [-625, 87, 3, -96, 44, 29, 999]
-    print(f'Original list:\n\t{unsorted_list}')
+    print(f'Original list: \t\t{unsorted_list}')
 
     bubble_sort(unsorted_list)  # Call the bubble sort function
-    print(f'Ordered list:\n\t{unsorted_list}')  # Print the sorted list
+    print(f'Ordered list: \t\t{unsorted_list}')  # Print the sorted list
 
 if __name__ == '__main__':
     try:
