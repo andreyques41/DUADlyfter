0bc25452cc58cf0ef37cff693fbfe44face1d45e
Andrey Quesada Mora <andreyques41@gmail.com>
2025-03-31 22:21:09 -0600

    Week16 ready

---
+++ week_16/tests/test_bubblesort_ejercicio1.py
@@ -0,0 +1,36 @@
+import pytest
+from exercises.bubblesort_ejercicio1 import bubble_sort
+
+def test_bubble_sort_returns_sorted_list_with_small_list():
+    # Arrange
+    input_list = [ 2, -6, 87, 0, -879, 89, 10]
+    sorted_list = [ -879, -6, 0, 2, 10, 87, 89]
+    # Act
+    result_list = bubble_sort(input_list)
+    # Assert
+    assert result_list == sorted_list
+
+def test_bubble_sort_returns_sorted_list_with_big_list():
+    # Arrange
+    input_list = [ 2, -6, 87, 0, -879, 89, 10]
+    sorted_list = [ -879, -6, 0, 2, 10, 87, 89]
+    # Act
+    result_list = bubble_sort(input_list)
+    # Assert
+    assert result_list == sorted_list
+
+def test_bubble_sort_throws_exception_list_with_empty_list():
+    # Arrange
+    input_list = []
+    # Act & Assert
+    with pytest.raises(ValueError, match="Input list cannot be empty"):
+        result_list = bubble_sort(input_list)
+
+def test_bubble_sort_throws_exception_with_invalid_input():
+    # Arrange
+    input_list = 's'
+    # Act & Assert
+    with pytest.raises(ValueError, match="The input must be a list of numbers"):
+        result_list = bubble_sort(input_list)
+
+
